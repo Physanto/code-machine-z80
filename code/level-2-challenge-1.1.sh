58c0d264234ab78d102edbe7e59bf74a8cbd3b33
Physanto <hackmanu6@gmail.com>
2024-07-19 22:57:24 -0500

    Adding firs changes of progress with challenges initial commit

---
+++ code/level-2-challenge-1.1.sh
@@ -0,0 +1,12 @@
+#ANIMATION OF 4 PIXELS
+
+#this code machine animation of 4 pixels in screen but it does very fast
+#to make is slower you must increment the number of instructions 76 = halt
+#this makes a interruption the process for show in screen.
+
+3E 88 32 00 C0 76 76 76 76 76 76 76 76 76 76 76
+3E 44 32 00 C0 76 76 76 76 76 76 76 76 76 76 76
+3E 22 32 00 C0 76 76 76 76 76 76 76 76 76 76 76
+3E 11 32 00 C0 76 76 76 76 76 76 76 76 76 76 76
+C3 00 40
+
